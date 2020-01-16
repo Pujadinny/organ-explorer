204965c791f3bf5d9c726143e82aab3491a9a4b4
Pujadinny <theedup456@gmail.com>
2020-01-16 17:34:27 +0100

    BaseUnity
    Aixó és la Base de Unity

---
+++ Organ-Explorer/Assets/StarterKit-MoveCamAI/StarterKit/Scripts/Camera/CameraTopDown.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTopDown : MonoBehaviour
+{
+	private GameObject player;
+	private Vector3 offsetPos;
+
+	void Start()
+	{
+		player = GameObject.FindWithTag("Player"); // Find the GameObject named Player
+		offsetPos = new Vector3(0, 10f, -8f); // Set the camera's offset position
+	}
+
+	void OnEnable()
+	{
+		gameObject.transform.parent = null; // This makes the camera a parent object rather than a child
+	}
+	
+	
+}
