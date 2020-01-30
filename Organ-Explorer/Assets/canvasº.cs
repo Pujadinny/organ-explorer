21af6bd946542d431572e6c5b52e52b2e8136bdb
Pujadinny <theedup456@gmail.com>
2020-01-30 22:17:29 +0100

    TheNewOrganExplorer
    Version2

---
+++ Organ-Explorer/Assets/canvasº.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canvasº: MonoBehaviour
+{
+
+    public GameObject Inventario;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Inventario.SetActive(!Inventario.activeInHierarchy);
+        }
+    }
+}
